df0387ea6b22f7550319137947899cba7c8d68dd
TanishqDsharma <tanishqsharma612@gmail.com>
2024-09-17 15:51:12 +0000

    Added Liquidity Deposit related tests

--- test/FluidSwapV2PairTest.t.sol
+++ test/FluidSwapV2PairTest.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 
 import {FluidSwapV2Pair} from "../src/FluidSwapV2Pair.sol";
-import {Test} from "../lib/forge-std/src/Test.sol";
+import {Test,console} from "../lib/forge-std/src/Test.sol";
 import {ERC20Mintable} from "./Mocks/ERC20Mintable.sol";
 
 
@@ -17,7 +17,66 @@ ERC20Mintable token1;
         
         token0 = new ERC20Mintable("USDC","USDC");
         token1 = new ERC20Mintable("USDT","USDT");
+        token0.mint(100 ether,address(this));
+        token1.mint(100 ether,address(this));
 
         fluidSwapV2Pair = new FluidSwapV2Pair(address(token0),address(token1));
     }
+
+
+    function testMintBootstrap() public{
+        token0.transfer(address(fluidSwapV2Pair), 1 ether);
+        token1.transfer(address(fluidSwapV2Pair), 1 ether);
+        
+        console.log("TotalSupply is before mint:", fluidSwapV2Pair.totalSupply());
+
+        fluidSwapV2Pair.mint();
+
+
+        console.log("TotalSupply is before mint:", fluidSwapV2Pair.totalSupply());
+       
+        assertEq(fluidSwapV2Pair.balanceOf(address(this)), 1 ether - 1000);
+        assertEq(fluidSwapV2Pair.totalSupply(), 1 ether);
+
+    }
+
+    function testPoolIsAlreadyHavingLiquidity() public {
+        token0.transfer(address(fluidSwapV2Pair), 1 ether);
+        token1.transfer(address(fluidSwapV2Pair), 1 ether);
+        
+        fluidSwapV2Pair.mint();
+
+        token0.transfer(address(fluidSwapV2Pair), 1 ether);
+        token1.transfer(address(fluidSwapV2Pair), 1 ether);
+
+        fluidSwapV2Pair.mint();
+
+        assertEq(fluidSwapV2Pair.balanceOf(address(this)), 2 ether - 1000);
+        assertEq(fluidSwapV2Pair.totalSupply(), 2 ether);
+
+    }
+
+    function testUnbalancedLiquidityDeposit() public{
+        token0.transfer(address(fluidSwapV2Pair), 1 ether);
+        token1.transfer(address(fluidSwapV2Pair), 1 ether);
+        
+        fluidSwapV2Pair.mint();
+        
+        assertEq(fluidSwapV2Pair.balanceOf(address(this)), 1 ether - 1000);
+        assertEq(fluidSwapV2Pair.totalSupply(), 1 ether);
+
+        console.log("Tokens in Reserve0 are:",fluidSwapV2Pair.reserve0());
+        console.log("Tokens in Reserve0 are:",fluidSwapV2Pair.reserve1());
+
+        token0.transfer(address(fluidSwapV2Pair), 10 ether);
+        token1.transfer(address(fluidSwapV2Pair),20 ether);
+
+        fluidSwapV2Pair.mint();
+        
+        
+        assertEq(fluidSwapV2Pair.balanceOf(address(this)), 11 ether - 1000 );
+
+
+    }
+
 }
