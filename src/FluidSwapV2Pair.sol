c8160a15171419140ae5c5a3027b2ee8765036a4
TanishqDsharma <tanishqsharma612@gmail.com>
2024-09-17 09:00:57 +0000

    Create FluidSwapV2Pair contract

---
+++ src/FluidSwapV2Pair.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {ERC20} from "../lib/solmate/src/tokens/ERC20.sol";
+import {Math} from "./libraries/Math.sol";
+
+contract FluidSwapV2Pair is ERC20, Math{
+
+}
