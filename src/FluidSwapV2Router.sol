35ea9508d0b8939871d7ccb676739ebd4b88ed33
TanishqDsharma <tanishqsharma612@gmail.com>
2024-09-19 10:30:55 +0000

    Added SwapExactTokensForTokesn

--- src/FluidSwapV2Router.sol
+++ src/FluidSwapV2Router.sol
@@ -17,6 +17,7 @@ contract FluidSwapV2Router {
 error InsufficientAAmount();
 error InsufficientBAmount();
 error SafeTransferFailed();
+error InsufficientOutputAmount();
 
 IFluidSwapV2Factory factory;
 
@@ -67,6 +68,44 @@ function addLiquidity(
 
 }
 
+/// @notice This function swaps an exact input amount (amountIn) for some output amount not smaller than amountOutMin
+/// @param amountIn Enter the amount of tokens you want to swap
+/// @param amountOutMin Minimum amount of tokens that you want to recive atlest
+/// @param path The path parameter is just an array of token addresses. If we want to swap TokenA for TokenB directly 
+///  the path will contain  only Token A and Token B addresses. If we want to swap Token A for Token C via Token B, the path will 
+///  contain: Token A address, Token B address, Token C address; the contract would swap Token A for Token B and then Token B 
+///  for Token C. 
+/// @param to Address to recive the tokens
+function swapExactTokensForTokens(
+    uint256 amountIn, 
+    uint256 amountOutMin, 
+    address[] calldata path, 
+    address to
+    )  public returns (uint256[] memory amounts){
+
+        // Calling getAmountsOut that will return an array of amounts. 
+        // The first element of the array contains the amount input token that went in. 
+        // The last element of the amounts array contains the amount of token that came out from the last swap. 
+        // NOTE: If the swap involves multiple swap for eg: DAI to WETH and then WETH to USDC then this amounts array will also
+        // contain outputs for the intermediate swaps
+        amounts = FluidSwapV2Library.getAmountsOut(address(factory), amountIn, path);
+        
+        //Checking the amount of last swap that came out is less than amountOutMin than the transaction will revert. This is the
+        // number that users specifies in and it tells the uniswap the minimum amount of tokens that users wants from this swap.
+        if (amounts[amounts.length - 1] < amountOutMin)
+            revert InsufficientOutputAmount();
+            
+        // Now, the input token is being transfered to the pair contract. The input token will be stored inside path[0] and 
+        // pair contract is computed by taking path[0] and path[1]. The way it computes the address is by using create2 and 
+        // the amount to send is stored inside amounts[0]
+        _safeTransferFrom(path[0],msg.sender,FluidSwapV2Library.pairFor(address(factory), path[0], path[1]),amounts[0]);
+        _swap(amounts, path, to);
+    
+    
+    }
+
+
+
 
 //////////////////////////////////// 
 //// Private and Internal Funcs //// 
@@ -98,6 +137,45 @@ function _calculateLiquidity(
         }
     }
 
+    /// @notice The _swap function performs a series of token swaps along a predefined path. For each step, it calculates how much of the
+    /// output token should be sent out and where the output tokens should be sent (either to the next pair in the path or the final recipient).
+    /// @param amounts The amounts here is same as the amounts param as seen in swapExactTokensForTokens which has first elements
+    /// as inputAmount , next the intemediary output amount and last element is the output amount for last swap.
+    /// @param path Is the address of the token to swap
+    /// @param to_  Receiver of the final output token
+    function _swap(
+        uint256[] memory amounts,
+        address[] memory path,
+        address to_
+    ) internal {
+        for (uint256 i; i < path.length - 1; i++) {
+            
+            // Input token will be the token at path[i] position and output token will be token at path[i+1] position
+            (address input, address output) = (path[i], path[i + 1]);
+            
+            //In this line we are doing sorting of the tokens, to determine the address which will be stores in token0. So here 
+            // Smaller address out of the two tokens will be stored in token0.The sorting ensures consistency in token pair references 
+            // in liquidity pools.
+            (address token0, ) = FluidSwapV2Library.sortTokens(input, output);
+            
+
+            //The amount of output tokens to be received is taken from amounts[i + 1]. 
+            //This represents how many of the next token (output) will be obtained from the swap.
+            uint256 amountOut = amounts[i + 1];
+            
+            // This is used to pass to the swap in the last line
+            (uint256 amount0Out, uint256 amount1Out) = input == token0 ? (uint256(0), amountOut) : (amountOut, uint256(0));
+            
+            // This line checks if this is not the final swap then the output is sent to next liquidity pair and if this is the 
+            // final swap the output should be sent to receiver address(the `to_` param).
+
+            address to = i < path.length - 2 ? FluidSwapV2Library.pairFor(address(factory),output,path[i + 2]): to_;
+            
+            IFluidSwapV2Pair(FluidSwapV2Library.pairFor(address(factory), input, output)).swap(amount0Out, amount1Out, to, "");
+        }
+    }
+
+
 
 function _safeTransferFrom(
         address token,
@@ -116,6 +194,8 @@ function _safeTransferFrom(
         if (!success || (data.length != 0 && !abi.decode(data, (bool))))
             revert SafeTransferFailed();
     }
+
+
 
 
 
