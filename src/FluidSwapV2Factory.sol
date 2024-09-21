30051246a212506d9601d7b994268b54b96d90a2
TanishqDsharma <tanishqsharma612@gmail.com>
2024-09-21 11:38:17 +0000

    Updated

--- src/FluidSwapV2Factory.sol
+++ src/FluidSwapV2Factory.sol
@@ -41,26 +41,41 @@ address[] public allPairs;
 ///// Public Functions ///
 ////////////////////////// 
 
+/**
+ * @notice This function deploys the uniswapV2Pair contract. 
+ * @param tokenA Pass address of tokenA 
+ * @param tokenB Pass address of tokenB
+ * @return pair returns address of the pair contract that was deployed
+ */
 
 function createPair(address tokenA, address tokenB) public returns(address pair){
+    
+    // Firstly, it checks that tokenA and tokenB are not same
     if(tokenA==tokenB){
         revert IdenticalAddresses();
     }
 
-    (address token0,address token1) = tokenA<tokenB?(tokenA,tokenB):(tokenB,tokenA);
+    // Next, it sorts the addresses based on their numerical values and returns token0 and token1. 
 
+    (address token0,address token1) = tokenA<tokenB?(tokenA,tokenB):(tokenB,tokenA);
+    
+    // Here, we are checking that token0 is not equal to address0
     if(token0==address(0)){
         revert ZeroAddress();
     }
 
+    // Next, we are checking that pair for these token addresses doesn't exists.
     if(pairs[token0][token1]!=address(0)) revert  PairExists();
 
+    // In this line, we are getting the creationcode of FluidSwapV2Pair contract.
     bytes memory bytecode = type(FluidSwapV2Pair).creationCode;
+    //Calculating the salt
     bytes32 salt = keccak256(abi.encodePacked(token0,token1));
     
-    assembly{
-        pair:=create2(0,add(bytecode,32),mload(bytecode),salt)
-    }
+    // using create2 for getting the address of FluidSwapV2Pair contract before its deployed
+     assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
 
     IFluidSwapV2Pair(pair).initialize(token0, token1);
 
